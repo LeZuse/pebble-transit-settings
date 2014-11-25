4081661d258dc22c05259568690db0f8f56fa219
Tomas Ruzicka <tomasruzicka@abdoc.net>
2014-11-25 07:56:34 +0100

    settings form

--- build/settings.js
+++ build/settings.js
@@ -1,21 +1,63 @@
-var s = ['']
-for(var i=0, len=localStorage.length; i<len; i++) {
-  var key = localStorage.key(i);
-  var value = localStorage[key];
-  s += key +' '+value+'\n';
-}
 
-try {
-var options = JSON.parse(decodeURIComponent(window.location.hash.substr(1)));
-} catch(e) {}
+var Form = React.createClass({displayName: 'Form',
+  getInitialState: function() {
+    var options;
+    try {
+      options = JSON.parse(decodeURIComponent(window.location.hash.substr(1)));
+    } catch(e) {}
+
+    if (!options || !options.journeys) {
+      options = options || {};
+      options.journeys = [];
+    }
+
+    return {
+      options: options
+    }
+  },
+  saveSettings: function(e) {
+    e.preventDefault();
+    document.querySelector('#main').innerHTML = '<center><h1>SAVED';
+    // console.log(this.state.options)
+    document.location = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(this.state.options));
+  },
+  addJourney: function(e) {
+    e.preventDefault();
+    this.state.options.journeys.push({name: '',form:'',to:''});
+    this.forceUpdate()
+  },
+  onChange: function(i) {
+    self = this
+    return function(e) {
+      self.state.options.journeys[i][e.target.name] = e.target.value;
+      self.forceUpdate()
+    }
+  },
+  render: function() {
+    self = this
+    return (
+      React.createElement("form", null, 
+        this.state.options.journeys.map(function(j, i){
+          return React.createElement("label", {key: i}, 
+            "Name ", React.createElement("input", {name: "name", type: "text", value: j.name, onChange: self.onChange(i)}), React.createElement("br", null), 
+            "From ", React.createElement("input", {name: "from", type: "text", value: j.from, onChange: self.onChange(i)}), 
+            "To ", React.createElement("input", {name: "to", type: "text", value: j.to, onChange: self.onChange(i)}), 
+            "Area ", React.createElement("input", {name: "location", type: "text", value: j.location, onChange: self.onChange(i)})
+          )
+        }), 
+        React.createElement("br", null), 
+        React.createElement("input", {type: "submit", value: "Add", onClick: this.addJourney}), 
+        React.createElement("input", {type: "submit", value: "Save", onClick: this.saveSettings})
+      )
+    );
+  }
+});
 
 React.render(
-  React.createElement("div", null, 
-    React.createElement("h1", null, "Hello, world!"), 
-    React.createElement("div", null, "LS items: ", localStorage.length), 
-    React.createElement("div", null, s), 
-    React.createElement("textarea", null, JSON.stringify(options)), 
-    window.location.hash
+  React.createElement("div", {id: "main"}, 
+    React.createElement("h1", {id: "head"}, "Transit Settings"), 
+
+    React.createElement(Form, null)
   ),
   document.getElementById('example')
 );
--- src/settings.js
+++ src/settings.js
@@ -1,21 +1,63 @@
-var s = ['']
-for(var i=0, len=localStorage.length; i<len; i++) {
-  var key = localStorage.key(i);
-  var value = localStorage[key];
-  s += key +' '+value+'\n';
-}
 
-try {
-var options = JSON.parse(decodeURIComponent(window.location.hash.substr(1)));
-} catch(e) {}
+var Form = React.createClass({
+  getInitialState: function() {
+    var options;
+    try {
+      options = JSON.parse(decodeURIComponent(window.location.hash.substr(1)));
+    } catch(e) {}
+
+    if (!options || !options.journeys) {
+      options = options || {};
+      options.journeys = [];
+    }
+
+    return {
+      options: options
+    }
+  },
+  saveSettings: function(e) {
+    e.preventDefault();
+    document.querySelector('#main').innerHTML = '<center><h1>SAVED';
+    // console.log(this.state.options)
+    document.location = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(this.state.options));
+  },
+  addJourney: function(e) {
+    e.preventDefault();
+    this.state.options.journeys.push({name: '',form:'',to:''});
+    this.forceUpdate()
+  },
+  onChange: function(i) {
+    self = this
+    return function(e) {
+      self.state.options.journeys[i][e.target.name] = e.target.value;
+      self.forceUpdate()
+    }
+  },
+  render: function() {
+    self = this
+    return (
+      <form>
+        {this.state.options.journeys.map(function(j, i){
+          return <label key={i}>
+            Name <input name="name" type="text" value={j.name} onChange={self.onChange(i)} /><br />
+            From <input name="from" type="text" value={j.from} onChange={self.onChange(i)} />
+            To <input name="to" type="text" value={j.to} onChange={self.onChange(i)} />
+            Area <input name="location" type="text" value={j.location} onChange={self.onChange(i)} />
+          </label>
+        })}
+        <br />
+        <input type="submit" value="Add" onClick={this.addJourney} />
+        <input type="submit" value="Save" onClick={this.saveSettings} />
+      </form>
+    );
+  }
+});
 
 React.render(
-  <div>
-    <h1>Hello, world!</h1>
-    <div>LS items: {localStorage.length}</div>
-    <div>{s}</div>
-    <textarea>{JSON.stringify(options)}</textarea>
-    {window.location.hash}
+  <div id="main">
+    <h1 id="head">Transit Settings</h1>
+
+    <Form />
   </div>,
   document.getElementById('example')
 );
