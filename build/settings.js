ee9c877ab140bd0ba4b0aae46563f641af358825
Tomas Ruzicka <tomasruzicka@abdoc.net>
2014-11-25 06:54:51 +0100

    localstorage dump

--- build/settings.js
+++ build/settings.js
@@ -1,7 +1,15 @@
+var s = ['']
+for(var i=0, len=localStorage.length; i<len; i++) {
+  var key = localStorage.key(i);
+  var value = localStorage[key];
+  s += key +' '+value+'\n';
+}
+
 React.render(
   React.createElement("div", null, 
     React.createElement("h1", null, "Hello, world!"), 
-    React.createElement("div", null, localStorage['journeys'])
+    React.createElement("div", null, "LS items: ", localStorage.length), 
+    s
   ),
   document.getElementById('example')
 );
--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,15 @@
+var s = ['']
+for(var i=0, len=localStorage.length; i<len; i++) {
+  var key = localStorage.key(i);
+  var value = localStorage[key];
+  s += key +' '+value+'\n';
+}
+
 React.render(
   <div>
     <h1>Hello, world!</h1>
-    <div>{localStorage['journeys']}</div>
+    <div>LS items: {localStorage.length}</div>
+    {s}
   </div>,
   document.getElementById('example')
 );
